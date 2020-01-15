d8259acee065c087fa467dd8b02ee28b0a30ca6e
Ermano Claude <ermano.claude@cix.csi.cuny.edu>
2020-01-15 12:18:18 -0500

    Update SymmetricDifference.py

--- SymmetricDifference.py
+++ SymmetricDifference.py
@@ -1,4 +1,4 @@
-"""  A function that takes two or more arrays and returns an array of the symmetric difference (△ or ⊕) of the provided arrays. """
+"""  A function that takes two or more arrays and returns an array of the symmetric difference (△ or ⊕). """
 
 def sym(*args):
 	"""Return Unique Values between Arrays into one array """
